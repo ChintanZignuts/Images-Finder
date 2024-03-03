ab32ba3ae6e10de3f6153062c0570c77b4f38efb
chintanrupareliya <ckrupareliya2002@gmail.com>
2024-03-04 00:27:32 +0530

    pagination added2

--- src/components/ImageCard.vue
+++ src/components/ImageCard.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
 defineProps<{
   images: Object
   error?: string
@@ -8,22 +6,34 @@ defineProps<{
 </script>
 <template>
   <div class="ma-10">
-    <!-- Check if there are images and no error -->
     <div v-if="images && !error">
       <v-row>
         <v-col
-          v-for="image of images.results || images"
+          v-for="image of images"
           :key="image.id"
           class="d-flex child-flex"
-          cols="4"
+          cols="12"
+          lg="4"
+          md="4"
+          sm="6"
+          xs="12"
         >
           <v-img
             :lazy-src="`https://picsum.photos/10/6?image=${3 * 5 + 10}`"
             :src="image.urls.regular"
             aspect-ratio="1"
-            class="bg-grey-lighten-2"
+            :alt="image.alt_description"
+            class="bg-grey-lighten-2 rounded-lg"
             cover
           >
+            <v-card-title class="text-h6 text-white d-flex flex-column">
+              <p class="mt-4">{{ image.user.name }}</p>
+
+              <div>
+                <p class="ma-0 text-body-1 font-weight-bold"></p>
+                <p class="text-caption font-weight-medium">sfsdfs</p>
+              </div>
+            </v-card-title>
             <template v-slot:placeholder>
               <v-row align="center" class="fill-height ma-0" justify="center">
                 <v-progress-circular color="grey-lighten-5" indeterminate></v-progress-circular>
@@ -33,15 +43,13 @@ defineProps<{
         </v-col>
       </v-row>
 
-      <!-- Display message if images.results is an empty array -->
-      <div v-if="!images.results || images.results.length === 0">
+      <div v-if="images.length <= 0">
         <v-alert type="warning" dismissible> Images not found. </v-alert>
       </div>
     </div>
 
-    <!-- Display error message if there is an error -->
     <div v-else-if="error">
-      {{ error }}
+      <v-alert type="warning" dismissible> {{ error }} </v-alert>
     </div>
   </div>
 </template>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,16 +4,39 @@ import InputForm from '@/components/InputForm.vue'
 import { getTrendingImg, getImgBySearch } from '../Service'
 import { ref } from 'vue'
 import { onMounted } from 'vue'
+import { watch } from 'vue'
 
-const images = ref({})
+const images = ref(null)
 const error = ref('')
+const page = ref(1)
+const searchedimg = ref(false)
+const searchedpages = ref(1)
+const totalpages = ref(1)
+const search = ref('')
+const totalresult = ref(0)
+
+const handalpagination = async (page: number) => {
+  images.value = await getTrendingImg(page)
+}
+const handalsearchpagination = async (page: number) => {
+  const Response = await getImgBySearch(search.value, page)
+  images.value = Response.results
+}
+
+watch(page, handalpagination)
+watch(searchedpages, handalsearchpagination)
+
 const handleSearch = async (searchValue: string) => {
-  console.log(searchValue)
-  images.value = await getImgBySearch(searchValue)
+  search.value = searchValue
+  const Response = await getImgBySearch(searchValue, searchedpages.value)
+  images.value = Response.results
+  totalpages.value = Response.total_pages
+  totalresult.value = Response.total
+  searchedimg.value = true
 }
 onMounted(async () => {
   try {
-    images.value = await getTrendingImg()
+    images.value = await getTrendingImg(page.value)
   } catch (e) {
     if (e) {
       error.value = 'error acured during get trending img'
@@ -21,7 +44,28 @@ onMounted(async () => {
   }
 })
 </script>
+
 <template>
   <InputForm @submit="handleSearch" />
-  <ImageCard :images="images" :error="error" />
+  <div v-if="searchedimg">
+    <h3>
+      Total Result:- <span>{{ totalresult }}</span>
+    </h3>
+  </div>
+  <div v-if="images">
+    <ImageCard :images="images" :error="error" />
+  </div>
+
+  <div>
+    <div v-if="!searchedimg">
+      <v-pagination :length="30" :total-visible="6" v-model="page"></v-pagination>
+    </div>
+    <div v-else>
+      <v-pagination
+        :length="totalpages < 30 ? totalpages : 30"
+        :total-visible="6"
+        v-model="searchedpages"
+      ></v-pagination>
+    </div>
+  </div>
 </template>
